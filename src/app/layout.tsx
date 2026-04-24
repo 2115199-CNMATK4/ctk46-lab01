0d585bd6254646f6ff924945b189e265d295bb75
Hoang Pham Anh Duy <ctk46@etylix.me>
2026-04-24 17:10:26 +0700

    style: change theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
-import { Geist } from "next/font/google";
+import { Geist, Roboto, Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Portfolio — Hoàng Pham Anh Duy",
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="vi" className={cn("font-sans", geist.variable)}>
+    <html lang="vi" className={cn("font-sans", inter.variable)}>
       <body className="min-h-screen flex flex-col">
         <Navbar />
         <main className="flex-1">{children}</main>
