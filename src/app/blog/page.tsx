b5af02ea9a1c10f4d7b45c4ff1247ebdd0ec6b85
Hoang Pham Anh Duy <ctk46@etylix.me>
2026-04-24 15:08:22 +0700

    Revert "test: no-cache for blog fetching"
    no-cache fetch api every time, no more caching for blog page, this make blog uses more network traffic and slower
    This reverts commit 5f3e197cf99b51d183623b2f06af0e6ce89a1551.

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { Post } from "@/types/post";
 
 async function getPosts(): Promise<Post[]> {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts", { cache: 'no-store' });
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
   if (!res.ok) {
     throw new Error("Không thể tải danh sách bài viết");
   }
